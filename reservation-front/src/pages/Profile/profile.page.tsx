4fc364215b69e0153f224b06f528378cbe215cc2
noureddine <noureddinelachgar9@gmail.com>
2023-07-21 10:38:07 +0100

    fix: remove warnings

--- reservation-front/src/pages/Profile/profile.page.tsx
+++ reservation-front/src/pages/Profile/profile.page.tsx
@@ -14,23 +14,22 @@ class ProfilePage extends Component {
                             <div className="card">
                                 <div className="card-body profile-card pt-4 d-flex flex-column align-items-center">
                                     <img
-                                        src="assets/img/profile-img.jpg"
                                         alt="Profile"
                                         className="rounded-circle"
                                     />
                                     <h2>Kevin Anderson</h2>
                                     <h3>Web Designer</h3>
                                     <div className="social-links mt-2">
-                                        <a href="#" className="twitter">
+                                        <a href="/#" className="twitter">
                                             <i className="bi bi-twitter" />
                                         </a>
-                                        <a href="#" className="facebook">
+                                        <a href="/#" className="facebook">
                                             <i className="bi bi-facebook" />
                                         </a>
-                                        <a href="#" className="instagram">
+                                        <a href="/#" className="instagram">
                                             <i className="bi bi-instagram" />
                                         </a>
-                                        <a href="#" className="linkedin">
+                                        <a href="/#" className="linkedin">
                                             <i className="bi bi-linkedin" />
                                         </a>
                                     </div>
@@ -143,17 +142,17 @@ class ProfilePage extends Component {
                                                         Profile Image
                                                     </label>
                                                     <div className="col-md-8 col-lg-9">
-                                                        <img src="assets/img/profile-img.jpg" alt="Profile" />
+                                                        <img alt="Profile" />
                                                         <div className="pt-2">
                                                             <a
-                                                                href="#"
+                                                                href="/#"
                                                                 className="btn btn-primary btn-sm"
                                                                 title="Upload new profile image"
                                                             >
                                                                 <i className="bi bi-upload" />
                                                             </a>
                                                             <a
-                                                                href="#"
+                                                                href="/#"
                                                                 className="btn btn-danger btn-sm"
                                                                 title="Remove my profile image"
                                                             >
