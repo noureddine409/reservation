e74fffb2d469de9b7fcb389e783bed434d7c5eb8
noureddine <noureddinelachgar9@gmail.com>
2023-07-21 10:32:14 +0100

    fix: remove warnings

--- reservation-front/src/pages/Error-404/error.page.tsx
+++ reservation-front/src/pages/Error-404/error.page.tsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import "./error.page.css"
+import {Link} from "react-router-dom";
 class ErrorPage extends Component {
     render() {
         return (
@@ -8,11 +9,10 @@ class ErrorPage extends Component {
                     <section className="section error-404 min-vh-100 d-flex flex-column align-items-center justify-content-center">
                         <h1>404</h1>
                         <h2>The page you are looking for doesn't exist.</h2>
-                        <a className="btn" href="index.html">
+                        <Link className="btn" to="/">
                             Back to home
-                        </a>
+                        </Link>
                         <img
-                            src="assets/img/not-found.svg"
                             className="img-fluid py-5"
                             alt="Page Not Found"
                         />
