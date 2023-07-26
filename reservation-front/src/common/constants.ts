03fd36685614d0c5d752c38bfbf50a2a110974eb
hajar-abouyahia <hajar20abouyahia@gmail.com>
2023-07-26 10:16:01 +0100

    feat/search-page

--- reservation-front/src/common/constants.ts
+++ reservation-front/src/common/constants.ts
@@ -5,3 +5,191 @@ export const ERROR_MESSAGES = {
     maxLength: 'Please enter at most {max} characters.',
     pattern: 'Invalid input.',
 };
+
+export const data = [
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "1",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "category": "Meeting room",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "2",
+        "description": "Sed do eiusmod tempor incididunt",
+        "category": "Equipments",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "3",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "4",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "5",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "category": "Meeting room",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "6",
+        "description": "Sed do eiusmod tempor incididunt",
+        "category": "Equipments",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "7",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "8",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "9",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "category": "Meeting room",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "10",
+        "description": "Sed do eiusmod tempor incididunt",
+        "category": "Equipments",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "11",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "12",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "1",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "category": "Meeting room",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "2",
+        "description": "Sed do eiusmod tempor incididunt",
+        "category": "Equipments",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "3",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "4",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "5",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "category": "Meeting room",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "6",
+        "description": "Sed do eiusmod tempor incididunt",
+        "category": "Equipments",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "7",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "8",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "9",
+        "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        "category": "Meeting room",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "10",
+        "description": "Sed do eiusmod tempor incididunt",
+        "category": "Equipments",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "11",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    },
+    {
+        "imgSrc": "https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg",
+        "name": "12",
+        "description": "Ut enim ad minim veniam, quis nostrud exercitation uex ea.",
+        "category": "Kitchen",
+        "status": "Availabale",
+    }
+]
+
+
+export const options = [
+    { value: 'Meeting room', label: 'Meeting room' },
+    { value: 'Kitchen', label: 'Kitchen' },
+    { value: 'Equipments', label: 'Equipments' },
+];
+
+export const DEFAULT_SEARCH_KEYWORD = ""
+export const DEFAULT_SEARCH_CATEGORY = {
+    label:"",
+    value:""
+  }
+export const DEFAULT_PAGE_NUMBER = 1
+export const DEFAULT_PAGE_SIZE =6
+
+
